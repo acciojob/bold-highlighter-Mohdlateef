976c68085fea9c6b4a4127855c503ac7139dcc2b
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-02-06 19:49:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+ const paragraph = document.querySelector('p');
 function highlight() {
     //Write your code here
+const boldElements = paragraph.querySelectorAll('strong');
 
+    // Change the color of bold elements to green
+    boldElements.forEach(element => {
+      element.style.color = 'green';
+    });
 
 }
 
 
 function return_normal() {
     //Write your code here
+	 const boldElements = paragraph.querySelectorAll('strong');
+
+    // Revert the color of bold elements back to black
+    boldElements.forEach(element => {
+      element.style.color = 'black';
+    });
 
     
 }
